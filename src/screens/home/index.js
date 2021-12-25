38cbc990df58e87e223f23eb7cfa495435a075b2
kamron-ctrl <81023205+kamron-ctrl@users.noreply.github.com>
2021-12-25 09:03:56 +0500

    Added home card (#1)
    * Added home card
    
    * Removed TouchableOpacity

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/core';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React from 'react'
+import { useNavigation } from '@react-navigation/core'
+import { View, Text, StyleSheet } from 'react-native'
+import HomeCard from './HomeCard'
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
 
-  const handleGoToFeed = () => navigation.navigate('Feed');
-  const handleGoToLogin = () => navigation.navigate('Login');
+  const handleGoToFeed = () => navigation.navigate('Feed')
+  const handleGoToLogin = () => navigation.navigate('Login')
 
   return (
     <View style={styles.main}>
-      <Text>Home</Text>
+      <HomeCard />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   main: {
     backgroundColor: '#ffffff',
     flex: 1,
+    paddingHorizontal: '5%',
   },
-});
+})
 
-export default HomeScreen;
+export default HomeScreen
